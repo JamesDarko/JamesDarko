fb46d87ebe54aa4545ec554aed67643b17335028
JamesDarko <59957970+JamesDarko@users.noreply.github.com>
2022-03-17 07:23:26 +0000

    Update hello_you.py
    Added strip to remove all unnecessary spaces in the input.

--- hello_you.py
+++ hello_you.py
@@ -1,21 +1,21 @@
 # Ask user for their name
-name = input("Your name would be nice: ")
+name = input("Your name would be nice: ").strip()
 print(name)
 
 # Ask user for their age
-age = input("How old are you?: ")
+age = input("How old are you?: ").strip()
 print(age)
 
 # Ask user for their town/city
-town = input("Where do you live?: ")
+town = input("Where do you live?: ").strip()
 print(town)
 
 # Ask user what they enjoy doing
-hobby = input("what do you enjoy doing?: ")
+hobby = input("what do you enjoy doing?: ").strip()
 print(hobby)
 
 # Ask user what they do
-occupation = input("What do you do?: ")
+occupation = input("What do you do?: ").strip()
 
 # Create output text
 string = "Hello {},you are {} years old. You live in {}. You are a {} but you love {}. Nice meeting you {}!"
