f9eb83ba2e1b175cf00472ed6fde832bc583660b
JamesDarko <59957970+JamesDarko@users.noreply.github.com>
2022-03-19 07:49:15 +0000

    Update hello_you.py
    Added name and region to make it more interactive and engaging.

--- hello_you.py
+++ hello_you.py
@@ -1,25 +1,29 @@
+# Introduction
+print("Hello there, I'm James Darko Amponsah...")
+
 # Ask user for their name
-name = input("Your name would be nice: ").strip()
-print(name)
+name = input("Your name would be nice: ").title()
 
 # Ask user for their age
-age = input("How old are you?: ").strip()
-print(age)
+age = input("{},how old are you?: ".format(name))
+print("Wow,that's good to know!")
 
 # Ask user for their town/city
-town = input("Where do you live?: ").strip()
-print(town)
+town = input("Where do you live?: ")
+
+# Ask user for their region
+region = input("{},where is {} located?(eg.Volta region): ".format(name,town))
 
 # Ask user what they enjoy doing
-hobby = input("what do you enjoy doing?: ").strip()
-print(hobby)
+hobby = input("what do you enjoy doing?: ")
 
 # Ask user what they do
-occupation = input("What do you do?: ").strip()
+occupation = input("What do you do?: ")
 
 # Create output text
-string = "Hello {},you are {} years old. You live in {}. You are a {} but you love {}. Nice meeting you {}!"
-output = string.format(name,age,town,occupation,hobby,name)
+string = "Hello {},you are {} years old and from {} in the {} region. You are a {} but you love {}. Nice meeting you {}!"
+output = string.format(name,age,town,region,occupation,hobby,name)
 
 # Print output to screen
 print(output)
+
